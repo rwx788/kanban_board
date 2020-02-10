17f58e69ed47d394f5833097384681d35adb4fe7
rwx788 <yafarovrs@gmail.com>
2020-02-10 18:28:12 +0100

    Split seed into multiple files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,4 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Status.destroy_all
-
-statuses_list = [
-    { status: 'Blocked' },
-    { status: 'Open' },
-    { status: 'In Progress' },
-    { status: 'Review' },
-    { status: 'Done' }
-]
-
-statuses_list.each do |status|
-    Status.create!(status: status)
-end
-
-p "Created #{Status.count} statuses"
+# Load seeds directly in seeds directory and any nested directory
+Dir[File.join(Rails.root, 'db', 'seeds/**/*.rb')].sort.each { |seed| load seed }
