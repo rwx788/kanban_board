ed47acaf4c906fe5bdcecd2572c9a1b3191fae30
Rodion Iafarov <riafarov@suse.com>
2020-02-10 18:07:16 +0100

    Add seeds for statuses
    Adding default statuses in the column. Tickets will be created as
    fixtures for test database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Status.destroy_all
+
+statuses_list = [
+    { status: 'Blocked' },
+    { status: 'Open' },
+    { status: 'In Progress' },
+    { status: 'Review' },
+    { status: 'Done' }
+]
+
+statuses_list.each do |status|
+    Status.create!(status: status)
+end
+
+p "Created #{Status.count} statuses"
