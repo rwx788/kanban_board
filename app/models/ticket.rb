1e7b2622caf65e97426dd4b8d808e95c0a2566e4
rwx788 <yafarovrs@gmail.com>
2020-02-11 15:19:59 +0100

    Add tickets controller and view
    First step to display tickets as a table sorted by order_nr.
    Next step will be to get draggable list, and modify order of the
    tickets.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,5 @@
 class Ticket < ApplicationRecord
     belongs_to :status
+    # Always sort by order and preserve it
+    default_scope { order(order_nr: :asc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'tickets/index'
+  root to: "tickets#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
