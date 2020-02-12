4365199dc608c59d57e5b079673eae5379ec9397
rwx788 <yafarovrs@gmail.com>
2020-02-12 17:35:38 +0100

    Rename status column from "status" to "name"

--- db/migrate/20200210160825_create_statuses.rb
+++ db/migrate/20200210160825_create_statuses.rb
@@ -1,7 +1,7 @@
 class CreateStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :statuses do |t|
-      t.string   :status, unique: true, index: true
+      t.string   :name, unique: true, index: true
     end
   end
 end
