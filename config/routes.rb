41175fb0f2ff8e49831320d07850b705a0a97464
rwx788 <yafarovrs@gmail.com>
2020-02-12 13:32:52 +0100

    Store position of the ticket in db when changed in UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'tickets/index'
-  root to: "tickets#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get 'tickets/index'
+    root to: 'tickets#index'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :tickets do
+        collection do
+            patch :sort
+        end
+    end
 end
