d08f462fee72653b2556f692a1c462be89f72f8a
Rodion Iafarov <riafarov@suse.com>
2020-02-13 18:46:54 +0100

    Allow moving tickets between columns in front-end

--- app/javascript/packs/tickets.js
+++ app/javascript/packs/tickets.js
@@ -1,10 +1,15 @@
 import 'jquery-ui'
 import 'jquery-ui/ui/widget'
 import 'jquery-ui/ui/widgets/sortable'
+import 'jquery-ui/ui/widgets/draggable'
+import 'jquery-ui/ui/widgets/droppable'
 import Rails from "@rails/ujs";
 
 $(document).on("turbolinks:load", function() {
     $(".tickets").sortable({
+        connectWith: ".tickets",
+        tolerance: "pointer",
+        items: "> .ticket",
         update: function(e, ui) {
             Rails.ajax({
                 url: $(this).data("url"),
@@ -12,5 +17,9 @@ $(document).on("turbolinks:load", function() {
                 data: $(this).sortable('serialize'),
             });
         }
+    });
+    $(".ticket").draggable({
+        connectToSortable: ".tickets",
+        revert: "invalid"
     });
 });
