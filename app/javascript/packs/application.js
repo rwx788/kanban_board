d5105d44f80cdc146b2923da7da2053ccb018c97
Rodion Iafarov <riafarov@suse.com>
2020-02-14 14:02:41 +0100

    Add backend part to update status on drop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("bootstrap");
+require('expose-loader?$!jquery')
 import "../stylesheets/application";
+
+// import $ from 'jquery';
+// global.$ = jQuery;
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
